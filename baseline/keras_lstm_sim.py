d92a2e7a3af8118f6a517172a82d93fdc51696c4
chongjiu <chongjiu.jin@tinno.com>
2018-04-27 08:51:21 +0800

    add cv

--- baseline/keras_lstm_sim.py
+++ baseline/keras_lstm_sim.py
@@ -1,12 +1,13 @@
+input_file="../input/process.csv"
 import pandas as pd
 import numpy as np
 import keras
 from keras.callbacks import EarlyStopping, ModelCheckpoint, Callback
-from sklearn.metrics import f1_score,recall_score,precision_score
+from sklearn.metrics import f1_score,recall_score,precision_score,accuracy_score
 
 MAX_TEXT_LENGTH=50
 MAX_FEATURES=72039
-embedding_dims=100
+embedding_dims=128
 dr = 0.2
 
 def get_model():
@@ -31,17 +32,6 @@ class F1ScoreCallback(Callback):
     def on_batch_begin(self, batch, logs={}):
         pass
 
-    def on_batch_end(self, batch, logs={}):
-        if (self.include_on_batch):
-            logs['f1_score'] = float('0')
-            logs['recall'] = float('0')
-            if (self.validation_data):
-                print(self.validation_data.shape)
-                print(self.validation_data[0].shape)
-                print(self.validation_data[1].shape)
-                predict = self.model.predict(self.validation_data[0], batch_size=self.predict_batch_size)
-                logs['recall'] = f1_score(self.validation_data[1], predict)
-                logs['f1_score'] = recall_score(self.validation_data[1], predict)
     def on_train_begin(self, logs={}):
         if not ('f1_score' in self.params['metrics']):
             self.params['metrics'].append('f1_score')
@@ -52,15 +42,72 @@ class F1ScoreCallback(Callback):
         logs['f1_score'] = float('0')
         logs['recall']=float('0')
         if (self.validation_data):
-            print(self.validation_data.shape)
-            print(self.validation_data[0].shape)
-            print(self.validation_data[1].shape)
-            predict=self.model.predict(self.validation_data[0], batch_size=self.predict_batch_size)
-            logs['recall'] = f1_score(self.validation_data[1], predict)
-            logs['f1_score'] = recall_score(self.validation_data[1],predict)
+            # print(len(self.validation_data))
+            # print(self.validation_data[0].shape)
+            # print(self.validation_data[1].shape)
+            print(self.validation_data[2].shape)
+            # print(self.validation_data[3].shape)
+            # print(self.validation_data[4].shape)
+            y_predict=self.model.predict([self.validation_data[0],self.validation_data[1]], batch_size=self.predict_batch_size)
+            # y_predict1 = 1 - y_predict
+            # y_predict = np.stack((y_predict, y_predict1), axis=1)
+            y_predict =  (y_predict>0.5).astype(int)
+            print(y_predict.shape)
+            print(np.sum(y_predict))
+            # y_predict=np.reshape(y_predict,newshape=(-1,))
+            # y_predict[0]=1
+            # print(y_predict)
+            logs['recall'] = accuracy_score(self.validation_data[2], y_predict)
+            logs['f1_score'] = recall_score(self.validation_data[2],y_predict)
+            print(logs['recall'],logs['f1_score'])
 
 
-input_file="../input/process.csv"
+from tqdm import tqdm
+import mmap
+
+def get_num_lines(file_path):
+    fp = open(file_path, "r+")
+    buf = mmap.mmap(fp.fileno(), 0)
+    lines = 0
+    while buf.readline():
+        lines += 1
+    return lines
+
+def get_embedding_matrix(word_index,Emed_path):
+    #    Glove Vectors
+    print('Indexing word vectors')
+    embeddings_index = {}
+    file_line=get_num_lines(Emed_path)
+    print('lines ',file_line)
+    with open(Emed_path,encoding='utf-8') as f:
+        for line in tqdm(f, total=file_line):
+            values = line.split()
+            word = ' '.join(values[:-128])
+            coefs = np.asarray(values[-128:], dtype='float32')
+            embeddings_index[word] = coefs
+    f.close()
+
+    print('Total %s word vectors.' % len(embeddings_index))
+    print('Preparing embedding matrix')
+    nb_words = min(MAX_FEATURES, len(word_index))
+    all_embs = np.stack(embeddings_index.values())
+    print(all_embs.shape)
+    emb_mean, emb_std = all_embs.mean(), all_embs.std()
+    embedding_matrix = np.random.normal(loc=emb_mean, scale=emb_std, size=(nb_words, embedding_dims))
+    # embedding_matrix = np.zeros((nb_words, embedding_dims))
+    for word, i in tqdm(word_index.items()):
+        if i >= MAX_FEATURES:
+            continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            # words not found in embedding index will be all-zeros.
+            embedding_matrix[i] = embedding_vector
+    # np.save(Emed_path, embedding_matrix)
+    print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix, axis=1) == 0))
+    return embedding_matrix
+
+
+
 df = pd.read_csv(input_file,encoding="utf-8")
 
 question1=df['question1'].values
@@ -79,7 +126,9 @@ seed=20180426
 cv_folds=10
 from sklearn.model_selection import StratifiedKFold
 skf = StratifiedKFold(n_splits=cv_folds, random_state=seed, shuffle=False)
-pred_oob = np.zeros(y.shape)
+pred_oob = np.zeros(shape=(len(y),1))
+print(pred_oob.shape)
+count=0
 for ind_tr, ind_te in skf.split(X_train_q1, y):
     x_train_q1 = X_train_q1[ind_tr]
     x_train_q2 = X_train_q2[ind_tr]
@@ -89,18 +138,32 @@ for ind_tr, ind_te in skf.split(X_train_q1, y):
     y_val=y[ind_te]
     model = get_model()
     early_stopping = EarlyStopping(monitor='val_loss', patience=5, mode='min')
-    bst_model_path =  'weight.h5'
+    bst_model_path =  'weight_%d.h5'%count
     model_checkpoint = ModelCheckpoint(bst_model_path, monitor='val_loss', mode='min',
                                        save_best_only=True, verbose=1, save_weights_only=True)
-    hist = model.fit([x_train_q1,x_train_q2], y_train,
-                     validation_data=([x_val_q1,x_val_q2], y_val),
-                     epochs=50, batch_size=256, shuffle=True,
-                     callbacks=[early_stopping, model_checkpoint,F1ScoreCallback()])
-    y_predict=model.predict([x_val_q1,x_val_q2])
+    # hist = model.fit([x_train_q1,x_train_q2], y_train,
+    #                  validation_data=([x_val_q1,x_val_q2], y_val),
+    #                  epochs=5, batch_size=256, shuffle=True,
+    #                  callbacks=[early_stopping, model_checkpoint])
+    model.load_weights(bst_model_path)
+    y_predict=model.predict([x_val_q1,x_val_q2],batch_size=256,verbose=1)
     pred_oob[ind_te]=y_predict
+    y_predict = (y_predict > 0.5).astype(int)
+    recall = recall_score(y_val, y_predict)
+    print(count,"recal", recall)
+    precision = precision_score(y_val, y_predict)
+    print(count,"precision", precision)
+    accuracy = accuracy_score (y_val, y_predict)
+    print(count,"accuracy ", precision)
+    f1 = f1_score(y_val, y_predict)
+    print(count,"f1", f1)
+    count+=1
+pred_oob = (pred_oob > 0.5).astype(int)
 recall=recall_score(y,pred_oob)
 print("recal",recall)
-precision=precision_score(y,y_predict)
+precision=precision_score(y,pred_oob)
 print("precision",precision)
-f1=f1_score(y,y_predict)
+accuracy=accuracy_score(y,pred_oob)
+print("accuracy",accuracy)
+f1=f1_score(y,pred_oob)
 print("f1",f1)
